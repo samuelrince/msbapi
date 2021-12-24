79bdaded7d08948de74831454d525c217faf1bfb
Samuel Rince <s@rince.me>
2021-12-24 15:56:01 +0100

    Use another torrent file to test `add_torrent_file` with a path

--- tests/test_command.py
+++ tests/test_command.py
@@ -12,7 +12,7 @@ def test_add_torrent_file():
 
 def test_add_torrent_file_with_path():
     """Test add a simple torrent file"""
-    with open('big-buck-bunny.torrent', 'rb') as f:
+    with open('cosmos-laundromat.torrent', 'rb') as f:
         file = f.read()
     response = add_torrent_file(torrent_file=file, path='/data/client_6729/outp/test/')
     assert response.status_code == 200
