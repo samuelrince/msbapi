5120906bd4a02a2a71c61e497ea74d59629b19a3
Samuel Rince <s@rince.me>
2021-12-27 15:12:45 +0100

    Change response to JSONResponse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 from msbapi.api import TorrentRequest, TorrentResponse, add_new_torrent
 
@@ -7,9 +8,9 @@ app = FastAPI()
 
 
 @app.post('/add-torrent')
-def add_torrent(torrent: TorrentRequest) -> TorrentResponse:
+def add_torrent(torrent: TorrentRequest) -> JSONResponse:
     r = add_new_torrent(torrent)
-    return r
+    return JSONResponse(content={'success': r.success})
 
 
 if __name__ == '__main__':
